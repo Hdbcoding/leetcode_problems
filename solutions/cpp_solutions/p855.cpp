c25107767085583a214b1573abf2842bd7615316
henry <henry.byrd@claricode.com>
2021-01-19 23:24:55 -0500

    Figured out a bunch of bugs. Almost there

--- solutions/cpp_solutions/p855.cpp
+++ solutions/cpp_solutions/p855.cpp
@@ -1,6 +1,7 @@
-#include <set> 
-#include <unordered_map> 
-#include <iostream> 
+#include <set>
+#include <unordered_map>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 // Exam Room - Medium
@@ -13,178 +14,173 @@ struct gap
     int end;
     // is full?
     bool full{false};
-    
-    gap *prev{nullptr};
-    gap *next{nullptr};
-    
-    const int length() const {
+
+    // "pointers" to the next gap
+    int prev{-1};
+    int next{-1};
+
+    const int length() const
+    {
         return end - start + 1;
     }
-    
-    const int middle() const {
-        return start + length() / 2;
+
+    const int halfLength() const
+    {
+        return length() / 2;
+    }
+
+    const int middle() const
+    {
+        return start + halfLength();
+    }
+
+    friend ostream &operator<<(ostream &s, const gap &g)
+    {
+        return s << "{ start:" << g.start << ", end:" << g.end << ", next:" << g.next << ", prev:" << g.prev << ", halfLength:" << g.halfLength() << " }";
     }
 };
 
 struct gapComparator
 {
-    bool operator()(gap *a, gap *b) const
+    bool operator()(const gap &a, const gap &b) const
     {
-        if (a->start == b->start) 
+        if (a.start == b.start)
             return false;
-        
-        // cout << "\t\ta: { start:" << a->start << ", end:" << a->end << ", length:" << a->length() << ", middle: " << a->middle() << " }" << endl;
-        // cout << "\t\tb: { start:" << b->start << ", end:" << b->end << ", length:" << b->length() << ", middle: " << b->middle() << " }" << endl;
 
-        int al = a->length(), bl = b->length();
-        // same length? return the earlier one
-        if (al == bl) {
-            // cout << "\t\tlength equal, return earlier gap" << endl;
-            return a->start < b->start;
-        }
-        
-        // different length? 
-        // different by one?
-        // choose the bigger one if it's even, 
-        // otherwise choose the earlier one
-        int diff = al - bl;
-        if (diff > 1) 
-        {
-            // cout << "\t\ta much bigger" << endl;
-            return true;
-        }
-        if (diff < -1) 
-        {
-            // cout << "\t\tb much bigger" << endl;
-            return false;
-        }
-        
-        if (diff == 1 && al % 2 == 0) 
-        {
-            // cout << "\t\ta slightly bigger, and even" << endl;
-            return true;
-        }
-        if (diff == -1 && bl % 2 == 0) 
-        {
-            // cout << "\t\tb slightly bigger, and even" << endl;
-            return false;
-        }
-        
-        // cout << "\t\tbigger is not even, return earlier" << endl;
-        return a->start < b->start;   
+
+        int ahl = a.halfLength(), bhl = b.halfLength();
+
+        // hack for my weird choice of the first index of the gap being full
+        if (!a.full)
+            ++ahl;
+        if (!b.full)
+            ++bhl;
+
+        // if half lengths are equal, return the earlier gap
+        // otherwise, return the bigger gap
+        bool aBetter = (ahl == bhl && a.start < b.start) || ahl > bhl;
+
+        // cout << "\t\tcomparing a: " << a << ", b: " << b << "  ---> " << aBetter << endl;
+
+        return aBetter;
     }
 };
 
-class ExamRoom {
+class ExamRoom
+{
     // ordered binary tree of gaps, ordered by the comparator above
-    set<gap*, gapComparator> gapSet;
-    // set<gap*> gapSet;
-    unordered_map<int, gap*> gapMap;
-    
-    void removeFromSet(gap *a){
-        auto it = gapSet.find(a);
-        gapSet.erase(it);
+    set<gap, gapComparator> gapSet;
+    // map of starting index to gap object
+    unordered_map<int, gap> startMap;
+
+    void insert(gap &g)
+    {
+        // cout << "\tinserting gap: " << g << endl;
+        startMap[g.start] = g;
+        gapSet.insert(g);
     }
-    
+
 public:
-    ExamRoom(int N) {
-        gap* initial = new gap{ 0, N - 1 };
-        gapSet.insert(initial);
-        gapMap[0] = initial;
+    ExamRoom(int N)
+    {
+        gap initial{0, N - 1};
+        insert(initial);
     }
-    
-    int seat() {
+
+    int seat()
+    {
+        // cout << "choosing a seat... " << endl;
         // choose the biggest gap
         auto it = gapSet.begin();
-        gap *biggest = *it;
-        
+        gap biggest = *it;
+        gapSet.erase(it);
+
+        // cout << "biggest gap was: " << biggest << endl;
+
         // only the gap for the first index is ever empty
-        if (!biggest->full){
-            biggest->full = true;
+        if (!biggest.full)
+        {
+            biggest.full = true;
+            insert(biggest);
             return 0;
         }
-        
-        // remove the biggest from the gapSet
-        gapSet.erase(it);
-        
+
+        // split the gap
+        gap next{biggest.middle(), biggest.end, true, biggest.start};
         // if no one is sitting after me, add to the end of the gap
-        if (biggest->next == nullptr) 
+        if (biggest.next == -1)
         {
-            int end = biggest->end;
-            gap *next = new gap{end, end, true};
-            
-            // update ptrs
-            next->prev = biggest;
-            biggest->next = next;
-            
-            // update bounds of biggest
-            biggest->end = end - 1;
-            
-            // update gapMap
-            gapMap[end] = next;
-
-            // update gapSet
-            gapSet.insert(next);
-            gapSet.insert(biggest);
-            
-            return end;
-        } 
-        else 
-        {
-            // otherwise, split the gap
-            int middle = biggest->middle();
-            gap *next = new gap{middle, biggest->end, true};
-            
-            // update ptrs
-            if (biggest->next != nullptr){
-                biggest->next->prev = next;
-                next->next = biggest->next;
-            }
-            biggest->next = next;
-            next->prev = biggest;
-            
-            // update bounds of biggest
-            biggest->end = middle - 1;
-
-            // update gapMap
-            gapMap[middle] = next;
-
-            // update gapSet
-            gapSet.insert(next);
-            gapSet.insert(biggest);
-
-            return middle;
+            // cout << "no gap after biggest, sitting at the end" << endl;
+            next.start = biggest.end;
         }
+        else
+        {
+            // update the old next to recognize the new prev
+            auto nextIt = startMap.find(biggest.next);
+            gap oldNext = nextIt->second;
+            gapSet.erase(oldNext);
+
+            // cout << "splitting the gap. old gap after biggest was: " << oldNext << endl;
+
+            oldNext.prev = next.start;
+            next.next = oldNext.start;
+            insert(oldNext);
+        }
+
+        biggest.next = next.start;
+        biggest.end = next.start - 1;
+
+        insert(biggest);
+        insert(next);
+
+        return next.start;
     }
-    
-    void leave(int p) {
-        auto it = gapMap.find(p);
-        gap* here = it->second;
-        
+
+    void leave(int p)
+    {
+        // cout << "vacating seat " << p << endl;
+        auto it = startMap.find(p);
+        gap here = it->second;
+        startMap.erase(it);
+        gapSet.erase(here);
+
         // if this is the first slot, don't actually remove it, just mark it empty
-        if (here->start == 0){
-            here->full = false;
+        if (here.start == 0)
+        {
+            // cout << "special rule for first slot, just mark it empty" << endl;
+            here.full = false;
+            insert(here);
             return;
         }
-        
+
         // if we have a prev ptr, update its length and re add it to the set
-        if (here->prev != nullptr)
+        if (here.prev != -1)
         {
             // need to also remove and re-add here->prev;
-            here->prev->end = here->end;
-            gapSet.erase(here->prev);
-            gapSet.insert(here->prev);
+            auto prevIt = startMap.find(here.prev);
+            gap prev = prevIt->second;
+            gapSet.erase(prev);
+
+            // cout << "updating previous gap to have the correct end: " << prev << endl;
+
+            prev.end = here.end;
+            prev.next = here.next;
+
+            insert(prev);
         }
-        
+
         // if we have a next ptr, make sure that knows its proper prev ptr
-        if (here->next != nullptr) {
-            here->next->prev = here->prev;
+        if (here.next != -1)
+        {
+            auto nextIt = startMap.find(here.next);
+            gap next = nextIt->second;
+            gapSet.erase(next);
+
+            // cout << "updating next gap to have the correct prev" << next << endl;
+
+            next.prev = here.prev;
+            insert(next);
         }
-        
-        // remove this location from the set
-        gapSet.erase(here);
-        gapMap.erase(it);
-        delete here;
     }
 };
 
@@ -195,29 +191,31 @@ public:
  * obj->leave(p);
  */
 
-int main(){
-    ExamRoom room(10);
+int main()
+{
+    // ExamRoom room(10);
+    // int seat = room.seat();
+    // seat = room.seat();
+    // seat = room.seat();
+    // seat = room.seat();
+    // room.leave(4);
+    // seat = room.seat();
 
-    cout << "taking a seat..." << endl;
-    int seat = room.seat();
-    cout << "took seat: " << seat << endl;
-
-    cout << "taking a seat..." << endl;
-    seat = room.seat();
-    cout << "seating: " << seat << endl;
-
-    cout << "taking a seat..." << endl;
-    seat = room.seat();
-    cout << "seating: " << seat << endl;;
-
-    cout << "taking a seat..." << endl;
-    seat = room.seat();
-    cout << "seating: " << seat << endl;;
-
-    cout << "booting 4" << endl;
-    room.leave(4);
-
-    cout << "taking a seat..." << endl;
-    seat = room.seat();
-    cout << "seating: " << seat << endl;;
+    // ["ExamRoom","seat","seat","seat","leave","leave","seat","seat","seat","seat","seat","seat","seat","seat","seat","leave","leave","seat","seat","leave","seat","leave","seat","leave","seat","leave","seat","leave","leave","seat","seat","leave","leave","seat","seat","leave"]
+    // [[10],[],[],[],0,4,[],[],[],[],[],[],[],[],[],0,4,[],[],7,[],3,[],3,[],9,[],0,8,[],[],0,8,[],[],2]
+    // expected: [null,0,9,4,null,null,0,4,2,6,1,3,5,7,8,null,null,0,4,null,7,null,3,null,3,null,9,null,null,0,8,null,null,0,8,null]
+    ExamRoom room2{10};
+    vector<bool> sits{true, true, true, false, false, true, true, true, true, true, true, true, true, true, false, false, true, true, false, true, false, true, false, true, false, true, false, false, true, true, false, false, true, true, false};
+    vector<int> indices{-1, -1, -1, 0, 4, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 4, -1, -1, 7, -1, 3, -1, 3, -1, 9, -1, 0, 8, -1, -1, 0, 8, -1, -1, 2};
+    for (int i = 0; i < sits.size(); ++i){
+        if (sits[i]){
+            cout << "choosing an empty seat" << endl;
+            int sit = room2.seat();
+            cout << "seat " << sit << " was chosen" << endl;
+        } else {
+            cout << "clearing seat " << indices[i] << endl;
+            room2.leave(indices[i]);
+            cout << "cleared seat " << indices[i] << endl;
+        }
+    }
 }
